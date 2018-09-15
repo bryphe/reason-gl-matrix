506f1bbb5df59996795ce3cd457bf0f2fadcf45d
Bryan Phelps <bryphe@outlook.com>
2018-09-15 14:49:31 -0700

    Start converting to reason

--- src/mat4.ml
+++
@@ -1,6 +0,0 @@
-type mat4
-
-external create: unit -> mat4 = "caml_mat4_create"
-external get: mat4 -> int -> float = "caml_mat4_get"
-
-external debug_print: mat4 -> float = "caml_mat4_debug_print"
